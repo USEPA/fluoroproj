a9090b2fcf88391961065524ec525a043466f785
Jeff Hollister <jeff.w.hollister@gmail.com>
2022-02-14 17:05:09 -0500

    getting closer

--- R/analysis.R
+++ R/analysis.R
@@ -8,11 +8,29 @@ source(here::here("R/functions.R"))
 # Read in data
 handheld_data <- read_csv(here("data/raw/cyanofluor_fluoroquik_data.csv"))
 phycoprobe_data <- merge_phycoprobe()
-extracted_data <- merge_extracted()
+extracted_chla_data <- merge_extracted_chla()
+#extracted_phyco_data <- merge_extracted_phyco()
 invivo_data <- merge_invivo()
 
 # Clean up data
 handheld_data <- clean_handheld(handheld_data)
 phycoprobe_data <- clean_phycoprobe(phycoprobe_data)
+extracted_chla_data <- clean_extracted(extracted_chla_data)#, extracted_phyco_data)
 
-fluoroproj_data <- bind_rows(handheld_data, phycoprobe_data)
+
+fluoroproj_data <- bind_rows(handheld_data, phycoprobe_data, extracted_chla_data)
+
+x <- fluoroproj_data %>% 
+  filter(variable == "chl") %>%
+  group_by(date, waterbody, instrument, variable, units) %>%
+  summarize(chl = mean(value, na.rm = TRUE)) %>%
+  ungroup() %>%
+  group_by(waterbody,instrument, units) %>%
+  summarize(chl = mean(chl)) %>%
+  ungroup() %>%
+  filter(units == "µg/L") %>%
+  tidyr::pivot_wider(id_cols = "waterbody", names_from = "instrument", 
+                     values_from = "chl") %>%
+  filter(complete.cases(.))
+
+  
--- R/functions.R
+++ R/functions.R
@@ -1,10 +1,21 @@
-#' Merge trilogy extracted data
+#' Merge/convert trilogy extracted chla data
 #' 
-merge_extracted <- function(){
+merge_extracted_chla <- function(){
   files <- c(list.files(here("data/raw/extracted chl/"), ".csv", 
-                        full.names = TRUE),
-             list.files(here("data/raw/extracted phyco/"), ".csv", 
                         full.names = TRUE))
+  extracted_data <- purrr::map_df(files, 
+                                  function(x) 
+                                    ce_convert_rfus(x, "ext_chla", "2021", 
+                                                    "ours", std_check = FALSE))
+  extracted_data
+}
+
+#' Merge/convert trilogy extracted chla data
+#' 
+merge_extracted_phyco <- function(){
+  files <- c(list.files(here("data/raw/extracted phyco/"), ".csv", 
+                        full.names = TRUE))
+  browser()
   extracted_data <- purrr::map_df(files, 
                                   function(x) 
                                     read_csv(x, na = c("", "NA", "na")))
@@ -38,11 +49,11 @@ merge_invivo <- function(){
 #' @param df cyanoflour/fluoroquick data frame
 clean_handheld <- function(df){
   handheld_data <- mutate(df, date = ymd(paste0(year, month, day)),
-                          parameter = tolower(parameter))
-  handheld_data <- select(handheld_data, date, waterbody, dup, rep = reps, instrument,
-                          method = `fresh/frozen1/frozen3`, parameter, units, 
-                          value)
-  handheld_data <- filter(handheld_data, parameter %in% c("chl", "chl blk", 
+                          variable = tolower(parameter))
+  handheld_data <- select(handheld_data, date, waterbody, dups = dup, reps, 
+                          instrument, method = `fresh/frozen1/frozen3`, 
+                          variable, units, value)
+  handheld_data <- filter(handheld_data, variable %in% c("chl", "chl blk", 
                                                           "pc", "pc blk"))
   handheld_data <- mutate(handheld_data, units = case_when(units == "ug/l" ~
                                                              "µg/L",
@@ -61,16 +72,34 @@ clean_phycoprobe <- function(df){
   phycoprobe_data <- mutate(phycoprobe_data, date = ymd(paste0(year, month, day)),
                             instrument = "phycoprobe", units = "µg/L")
   #Assuming this data is what we want from phycoprobe
-  phycoprobe_data <- select(phycoprobe_data, date, waterbody, dup, rep = reps, instrument,
-                            method = `fresh/frozen1`, units,
+  phycoprobe_data <- select(phycoprobe_data, date, waterbody, dups = dup, reps, 
+                            instrument, method = `fresh/frozen1`, units,
                             chl = `total conc.`, bluegreen = `bluegreen...8`)
   phycoprobe_data <- pivot_longer(phycoprobe_data, cols = chl:bluegreen, 
-                                  names_to = "parameter", values_to = "value")
-  phycoprobe_data <- select(phycoprobe_data, date:method, parameter, units, 
+                                  names_to = "variable", values_to = "value")
+  phycoprobe_data <- select(phycoprobe_data, date:method, variable, units, 
                             value)
   phycoprobe_data <- mutate(phycoprobe_data, value = as.numeric(value))
   #borrowed from https://stackoverflow.com/questions/54221280/how-to-declare-encoding-for-all-character-columns-in-a-data-frame
   phycoprobe_data <- dplyr::mutate_if(phycoprobe_data, is.character, .funs = 
                                   function(x){return(`Encoding<-`(x, "UTF-8"))})
   phycoprobe_data
+}
+
+#' Clean extracted data
+#' 
+#' @param df merged extracted data
+clean_extracted <- function(df){
+  #browser()
+  extracted_data <- mutate(df, method = "extracted", instrument = "trilogy")
+  extracted_data <- select(extracted_data, date, waterbody, dups, reps, 
+                           instrument, method, variable, units, value)
+  extracted_data <- mutate(extracted_data, 
+                           variable = case_when(variable == "ext_chla" ~
+                                                  "chl",
+                                                TRUE ~ variable),
+                           waterbody = case_when(waterbody == "windmist farm" ~
+                                                   "windmist", 
+                                                 TRUE ~ waterbody))
+  extracted_data
 }
